8216272cfce888c75c0987042d86c7fcd2ed7c1a
Viktor Chiarcos <viktor.chiarcos@gmail.com>
2026-09-14 07:52:00 +0200

    update on Linux-Viktor from viktor-chiarcos with git-send

--- beta/assets/index-CX6URE9A.js
+++ beta/assets/index-CX6URE9A.js
@@ -1041,15 +1041,26 @@ var e=Object.defineProperty,t=(t,n)=>{let r={};for(var i in t)e(r,i,{get:t[i],en
           <div class="navbar">
             <div class="navbar-bg"></div>
             <div class="navbar-inner">
-              <div class="title">Popup</div>
+              <div class="title">Herzlich Willkommen zu VStore 3 beta</div>
               <div class="right">
-                <a href="#" class="link popup-close">Close</a>
+                <a href="#" class="link popup-close">Ok</a>
               </div>
             </div>
           </div>
           <div class="page-content">
             <div class="block">
-              <p>Popup content goes here.</p>
+            	<p>Was gibt's neues?</p>
+            </div>
+			<br>
+			<div class="block">
+            	<ul><li>VStore - Jetzt mit Liquid Glass</li></ul>
+            </div>
+			<br>
+			<div class="block">
+            	<p>Infos zur Beta:</p>
+            </div>
+			<div class="block">
+            	<p><b>Aktuelle Beta:</b> VStore 3 Developer Beta</p>
             </div>
           </div>
         </div>
