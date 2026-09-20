bb48654345cb0eae8fb2139284b19dad13644845
Viktor Chiarcos <viktor.chiarcos@gmail.com>
2026-09-20 10:10:19 +0200

    update on Linux-Viktor from viktor-chiarcos with git-send

--- beta/assets/index-CX6URE9A.js
+++ beta/assets/index-CX6URE9A.js
@@ -282,7 +282,7 @@ Each tab/view may have different layout, different navbar type (dynamic, fixed o
         <a href="#" class="button button-fill panel-open" data-panel="left">Left Panel</a>
         <a href="#" class="button button-fill panel-open" data-panel="right">Right Panel</a>
     	<!-- <a href="javascript:klaro.show();" class="button button-round button-outline">Klaro</a> -->
-      <a class="button is-success" onclick="banner.style.display = 'block'">Cookie-Einstellungen</a> 
+      <a class="button is-success" onclick="window.Cookies.set('cookie-consent', '', { expires: 365, secure: true, sameSite: 'strict' } ; location.href() = "">Cookie-Einstellungen</a> 
 
       </div>
 
